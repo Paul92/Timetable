cfdd76d547ff4df800243f176ab8c3f077854f65
Paul92 <paul.gafton@gmail.com>
2013-09-13 00:47:57 +0300

    Added database functionality to schedule page

--- src/pages/schedule.php
+++ src/pages/schedule.php
@@ -7,19 +7,42 @@ if(!$DB){
 
 $schedule = mysqli_query($DB, "SELECT * FROM schedule");
 
-echo '<form method="POST">';
+if(isset($_POST['submit'])){
+    while($day = mysqli_fetch_array($schedule)){
+        $startH = $day['dayName']."_startHour";
+        $endH   = $day['dayName']."_endHour";
 
-echo '<table>';
-echo '<tr><td>Day name</td><td>Start hour</td><td>End hour</td>';
+        $query = "UPDATE schedule SET ";
+        $query.= "startHour='" . $_POST[$startH]."', ";
+        $query.= "endHour='".$_POST[$endH]."' ";
+        $query.= "WHERE dayId='".$day['dayId']."'";
+
+        $out = mysqli_query($DB, $query);
+    }
+    $schedule = mysqli_query($DB, "SELECT * FROM schedule");
+}
+
+
+?>
+
+
+
+
+<form method="POST">
+
+<table>
+<tr><td>Day name</td><td>Start hour</td><td>End hour</td>
+
+<?php
 while($day = mysqli_fetch_array($schedule)){
     echo '<tr>';
     echo '<td><b>'.$day['dayName'].'</b></td>';
-    echo '<td>';
-    echo '<input type="text" name="'.$day['dayName'].'_startHour" value="'.$day['startHour'].'"/>';
-    echo '</td><td>';
-    echo '<input type="text" name="'.$day['dayName'].'_endHour" value="'.$day['endHour'].'"/>';
-    echo '</td></tr>';
-
+    echo '<td><input type="text" name="'.$day['dayName'].'_startHour" value="'.$day['startHour'].'"/></td>';
+    echo '<td><input type="text" name="'.$day['dayName'].'_endHour" value="'.$day['endHour'].'"/></td>';
+    echo '</tr>';
 }
-echo '</table>';
+?>
+
+</table>
+<input type="submit" name="submit" value="Submit"/>
 
