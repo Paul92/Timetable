82cd5057d51672ce34781e1854e1287f0bcb83aa
Paul92 <paul.gafton@gmail.com>
2013-09-12 14:39:18 +0300

    Added some error checking

--- src/index.php
+++ src/index.php
@@ -1,22 +1,34 @@
 <?php
 
-require_once 'functions.php';
-$pages=require_once 'pages.php';
+const CORE_FUNCTIONS_NOT_FOUND = "ERROR: functions.php not found\n";
+const CORE_PAGES_NOT_FOUND = "ERROR: pages.php not found\n";
 
-if(isset($_GET['show'])){
-    if(array_key_exists($_GET['show'], $pages))
-        $page=$_GET['show'];
-    else
-        $page='notfound';
+if(!file_exists('functions.php') && !is_readable('functions.php')){
+    echo CORE_FUNCTIONS_NOT_FOUND;
 }else{
-    $page='home';
+    require_once 'functions.php';
+
+    if(!file_exists('pages.php') && !is_readable('pages.php')){
+        echo CORE_PAGES_NOT_FOUND;
+    }else{
+        $pages=require_once 'pages.php';
+
+        if(isset($_GET['show'])){
+            if(array_key_exists($_GET['show'], $pages))
+                $page=$_GET['show'];
+            else
+                $page='notfound';
+        }else{
+            $page='home';
+        }
+
+        $menu=build_menu_from_pages($pages, $page);
+
+        $page_and_menu=array(
+            'menu' => $menu, 
+            'content' => $page
+        );
+
+        render('layout.php', $page_and_menu);
+    }
 }
-
-$menu=build_menu_from_pages($pages, $page);
-
-$page_and_menu=array(
-    'menu' => $menu, 
-    'content' => $page
-);
-
-render('layout.php', $page_and_menu);
