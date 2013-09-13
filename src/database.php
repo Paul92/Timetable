f2240bf4d82e075407f68339e56561db30cf524b
Paul92 <paul.gafton@gmail.com>
2013-09-13 22:04:02 +0300

    Doc for database module

---
+++ src/database.php
@@ -0,0 +1,20 @@
+/**
+ * This is the database module. It handels all database operations.
+ * There are 4 basic database operations: connect, read, write and disconnect,
+ * everyone handled by a function with same name.
+ *
+ * mysqli connect();
+ *
+ *      Returns a mysqli object used for database connection.
+ *
+ * mysqli_result fetch_table(string tableName);
+ *
+ *      Fetches tableName from database.
+ *
+ * disconnect(mysqli db);
+ *
+ *      Disconnects db database.
+ *
+ */
+
+
