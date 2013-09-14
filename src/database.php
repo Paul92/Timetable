6bb3b26f219358d9c1402a5880c80d411b17f0bc
Paul92 <paul.gafton@gmail.com>
2013-09-14 17:18:43 +0300

    Added connect()

--- src/database.php
+++ src/database.php
@@ -1,3 +1,4 @@
+<?php
 /**
  * This is the database module. It handels all database operations.
  * There are 4 basic database operations: connect, read, write and disconnect,
@@ -7,10 +8,6 @@
  *
  *      Returns a mysqli object used for database connection.
  *
- * mysqli_result fetch_table(string tableName);
- *
- *      Fetches tableName from database.
- *
  * disconnect(mysqli db);
  *
  *      Disconnects db database.
@@ -18,3 +15,11 @@
  */
 
 
+function connect(){
+    static $DB;
+    $DB = mysqli_connect('localhost', 'mysql', '123456', 'timetable');
+    if(!$DB)
+        exit('Connect error '.mysqli_connect_errno());
+
+    return $DB;
+}
