ec471dbd07f2dde6519bd2c4d57be9ebecfd0560
Paul92 <paul.gafton@gmail.com>
2013-09-17 19:53:46 +0300

    Removed database-courses modules dependency

--- src/pages/courses/courses_BL.php
+++ src/pages/courses/courses_BL.php
@@ -1,13 +1,5 @@
 <?php
 
-if(!file_exists('database.php') || !is_readable('database.php')){
-    exit(DATABASE_MODULE_NOT_FOUND);
-}
-
-require_once('database.php');
-
-$DB = connect();
-
 if(isset($_POST['submit'])){
     $query = "INSERT INTO courses (courseName) VALUES ('".$_POST['newCourse']."')";
     mysqli_query($DB, $query);
