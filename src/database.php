dd92da445ef7b917547c054b3a2a469eb9c301a1
Paul92 <paul.gafton@gmail.com>
2013-09-18 21:39:49 +0300

    Added documentation and improved formatting

--- src/database.php
+++ src/database.php
@@ -1,29 +1,38 @@
 <?php
 /**
  * This is the database module. It handels all database operations.
- * There are 4 basic database operations: connect, read, write and disconnect,
- * everyone handled by a function with same name.
- *
- * mysqli connect();
- *
- *      Returns a mysqli object used for database connection.
- *
- * void disconnect);
- *
- *      Disconnects database.
- *
  */
 
+/**
+ * Constants:
+ *
+ * HOST - database host
+ * USER - database user
+ * PASS - database password
+ * DB_NAME - database name
+ */
+const HOST 'localhost'
+const USER 'mysql'
+const PASS '123456'
+const DB_NAME 'timetable'
 
+/**
+ * Connect to database
+ *
+ * @return mysqli DB
+ */
 function connect(){
     static $DB;
     $DB = mysqli_connect('localhost', 'mysql', '123456', 'timetable');
-    if(!$DB)
+    if (!$DB) {
         exit('Connect error '.mysqli_connect_errno());
-
+    }
     return $DB;
 }
 
+/**
+ * Disconnect from database
+ */
 function disconnect(){
     mysqli_close(connect());
 }
