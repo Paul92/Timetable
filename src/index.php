54ab35ae3721278e79ac7e532f1a753a3f04a7d0
Paul92 <paul.gafton@gmail.com>
2013-09-18 21:29:14 +0300

    Improved formatting

--- src/index.php
+++ src/index.php
@@ -10,17 +10,17 @@ const CORE_FUNCTIONS_NOT_FOUND = "ERROR: functions.php not found\n";
 const CORE_PAGES_NOT_FOUND = "ERROR: pages.php not found\n";
 const DATABASE_MODULE_NOT_FOUND = "ERROR: database.php not found\n";
 
-if(!file_exists('functions.php') && !is_readable('functions.php')){
+if (!file_exists('functions.php') && !is_readable('functions.php')) {
     exit(CORE_FUNCTIONS_NOT_FOUND);
 }
 require_once 'functions.php';
 
-if(!file_exists('pages.php') && !is_readable('pages.php')){
+if (!file_exists('pages.php') && !is_readable('pages.php')) {
     exit(CORE_PAGES_NOT_FOUND);
 }
 $pages=require_once 'pages.php';
 
-if(!file_exists('database.php') || !is_readable('database.php')){
+if (!file_exists('database.php') || !is_readable('database.php')) {
     exit(DATABASE_NOT_FOUND);
 }
 require_once 'database.php';
@@ -33,24 +33,25 @@ $DB = connect();
 /**
  * Page to be shown. Default is home
  */
-$page='home';
+$page = 'home';
 
-if(isset($_GET['show'])){
-    if(array_key_exists($_GET['show'], $pages))
-        $page=$_GET['show'];
-    else
-        $page='notfound';
+if (isset($_GET['show'])) {
+    if (array_key_exists($_GET['show'], $pages)) {
+        $page = $_GET['show'];
+    } else {
+        $page = 'notfound';
+    }
 }
 
 /**
  * $menu holds the menu shown on page
  */
-$menu=build_menu_from_pages($pages, $page);
+$menu = build_menu_from_pages($pages, $page);
 
 /**
  * This array is used to pass variables to page modules
  */
-$vars=array(
+$vars = array(
     'menu' => $menu, 
     'content' => $pages[$page]['content'],
     'title' => $pages[$page]['title'],
