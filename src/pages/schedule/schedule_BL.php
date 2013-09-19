3871bb224b5ee85f41a0479f3e6338ee8216f85b
Paul92 <paul.gafton@gmail.com>
2013-09-19 20:38:25 +0300

    Improved formatting, database dependency and added documentation

--- src/pages/schedule/schedule_BL.php
+++ src/pages/schedule/schedule_BL.php
@@ -1,28 +1,58 @@
 <?php
+/**
+ * Schedule module. It handles schedule viewing and changing
+ */
 
-const TABLE_NAME schedule
+if (!file_exists(DATABASE_MODULE) || !is_readable(DATABASE_MODULE)) {
+    exit(DATABASE_MODULE_NOT_FOUND);
+}
+require_once(DATABASE_MODULE);
 
+/**
+ * Constants:
+ * TABLE_NAME - name of the schedule table
+ */
+const TABLE_NAME = 'schedule';
+
+/**
+ * Get schedule table from database
+ *
+ * @return mysqli_result $schedule schedule table
+ */
 function getSchedule(){
-    return mysqli_query($DB, "SELECT * FROM TABLE_NAME");
+    $DB = connect();
+    return mysqli_query($DB, "SELECT * FROM ".TABLE_NAME);
 }
 
+/**
+ * Update schedule table from database
+ *
+ * @param str $startH start hour
+ * @param str $endH   end hour
+ * @param str $dayId  id of day to update
+ *
+ * @return bool $ok true if query succeeded
+ */
 function updateSchedule($startH, $endH, $dayId){
+    $DB = connect();
     $query = "UPDATE TABLE_NAME SET ";
     $query.= "startHour='" . $_POST[$startH]."', ";
     $query.= "endHour='".$_POST[$endH]."' ";
     $query.= "WHERE dayId='".$day['dayId']."'";
+    mysqli_query($DB, $query);
 }
 
+/**
+ * Variabile to hold schedule table
+ */
 $schedule = getSchedule();
 
-if(isset($_POST['submit'])){
-    while($day = mysqli_fetch_array($schedule)){
+if (isset($_POST['submit'])) {
+    while ($day = mysqli_fetch_array($schedule)) {
         $startH = $_POST[$day['dayName']."_startHour"];
         $endH   = $_POST[$day['dayName']."_endHour"];
         updateSchedule($startH, $endH, $dayId);
-        $out = mysqli_query($DB, $query);
     }
     $schedule = getSchedule();
 }
 
-?>
