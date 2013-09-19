bc5fe9465cad2d2fda8d02cb4ba556af11cd1c9a
Paul92 <paul.gafton@gmail.com>
2013-09-19 20:07:14 +0300

    Added documentation

--- src/pages/courses/courses_BL.php
+++ src/pages/courses/courses_BL.php
@@ -1,9 +1,19 @@
 <?php
+/**
+ * Courses module. It handles course viewing and addition
+ */
+
+/**
+ * Constants:
+ */
 
 if(isset($_POST['submit'])){
     $query = "INSERT INTO courses (courseName) VALUES ('".$_POST['newCourse']."')";
     mysqli_query($DB, $query);
 }
 
+/**
+ * Variable for holding table courses
+ */
 $courses = mysqli_query($DB, "SELECT * FROM courses");
 
