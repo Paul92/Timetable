6be30a65154c9866329c873f9f8375b1d03ccc66
Paul92 <paul.gafton@gmail.com>
2013-09-23 20:12:30 +0300

    Fixed database uptdate error and added redirecting

--- src/pages/courses/addSubject.php
+++ src/pages/courses/addSubject.php
@@ -4,16 +4,17 @@
  * update
  */
 if(isset($_POST['subjectName'])){
-    $query = "INSERT INTO subjects (subjectName, courseId) VALUES (";
-    $query.= $_POST['subjectName']." ,".$_POST['courseId'];
+    $query = 'INSERT INTO subjects (subjectName, courseId) VALUES ("';
+    $query.= $_POST['subjectName'].'", '.$_POST['courseId'].');';
     $DB = connect();
     mysqli_query($DB, $query);
+    header('Location: ?show=courses');
 }
 
 
 if(isset($_GET['courseName'])){
     echo "<p>Add a subject to ".$_GET['courseName']." course</p>\n";
-    echo "<form method='GET'>\n";
+    echo "<form method='POST'>\n";
     echo "<label for='subjectName'>Subject Name: </label>\n";
     echo "<input type='hidden' name='courseId' value='".$_GET['courseId']."'/>\n";
     echo "<input type='text' id='subjectName' name='subjectName' />\n";
