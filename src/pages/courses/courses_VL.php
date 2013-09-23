5554bf379b6edafa0ce7bc2985aea2d7c005f8eb
Paul92 <paul.gafton@gmail.com>
2013-09-23 20:30:40 +0300

    Fixed html output for displaying correctly course subjects and used alternative syntax for echo

--- src/pages/courses/courses_VL.php
+++ src/pages/courses/courses_VL.php
@@ -7,18 +7,18 @@
 <?php
 while($course = mysqli_fetch_array($courses)){
     echo "<tr>\n";
-    echo "<td> Course ".$course['courseId'].': <a href="pages/courses/addSubject.php?courseName='.$course['courseName'];
-    echo '&amp;courseId='.$course['courseId'].'">'.$course['courseName']."</a></td>\n";
-    $subjects = mysqli_query($DB, "SELECT id FROM subjects WHERE courseID='".$course['courseId']."'");
-    if(/*mysqli_num_rows*/($subjects)){
-        echo "<table>\n";
+    echo '<td> Course ', $course['courseId'], ': <a href="?show=addSubject&amp;courseName=', $course['courseName'];
+    echo '&amp;courseId=', $course['courseId'], '">', $course['courseName'], "</a></td></tr>\n";
+    $subjects = mysqli_query($DB, "SELECT subjectName FROM subjects WHERE courseId='".$course['courseId']."'");
+    if($subjects){
+        echo "<tr><td><table style='margin-left: 25px'>\n";
         while($subject = mysqli_fetch_array($subjects)){
-            echo "<tr>";
-            echo "<td>".$subject['subjectName'].'</td>';
+            echo '<tr>';
+            echo '<td>', $subject['subjectName'], '</td>';
             echo "</tr>\n";
         }
+        echo "</table></td></tr>\n";
     }
-    echo "</tr>\n";
 }
 ?>
 
