cd084edd65b67aeee8197e1a82bd36e8af466e40
Paul92 <paul.gafton@gmail.com>
2013-09-29 21:18:57 +0300

    Improved mvc implementation

--- src/pages/courses/courses_BL.php
+++ src/pages/courses/courses_BL.php
@@ -7,9 +7,14 @@
  * Constants:
  */
 
+//database connection
 require_once(DATABASE_MODULE);
-
 $DB = connect();
+
+/**
+ * Data passed to VL
+ */
+$data = array();
 
 if (isset($_POST['submit'])) {
     $query = "INSERT INTO courses (courseName) VALUES ('".$_POST['newCourse']."')";
@@ -36,8 +41,18 @@ if (isset($_POST['deleteCourse'])) {
     }
 }
 
-/**
- * Variable for holding table courses
- */
+//$data['courses'] contains an associative array with course data, generated
+//by mysqli_fetch_all.
+//$data[i] contains a mysqli_result object that represents the subject data
+//for course i
 $courses = mysqli_query($DB, "SELECT * FROM courses");
 
+$data['courses'] = mysqli_fetch_all($courses, MYSQLI_ASSOC);
+foreach($data['courses'] as $course){
+    $query  = "SELECT * FROM subjects WHERE courseId=".$course['courseId'];
+    $result = mysqli_query($DB, $query);
+    $data[$course['courseId']] = $result;
+}
+
+//return to controller
+return $data;
--- src/pages/courses/courses_VL.php
+++ src/pages/courses/courses_VL.php
@@ -1,27 +1,31 @@
-<?php include 'courses_BL.php'; ?>
-
 <form method="POST">
 
 <table>
 
 <?php
-while($course = mysqli_fetch_array($courses)){
+var_dump($data);
+foreach($data['courses'] as $course){
     echo "<tr>\n";
-    echo '<td> Course ', $course['courseId'], ': <a href="?show=addSubject&amp;courseName=', $course['courseName'];
-    echo '&amp;courseId=', $course['courseId'], '">', $course['courseName'], "</a></td>\n";
-    echo '<td> <button type="submit" name="deleteCourse[]" value="', $course['courseId'], '">Delete</button></td>';
-    echo '</tr>';
-    $subjects = mysqli_query($DB, "SELECT * FROM subjects WHERE courseId='".$course['courseId']."'");
-    if($subjects){
+    echo '<td> Course ', $course['courseId'], ':';
+    echo '<a href="?show=addSubject&amp;courseName=', $course['courseName'];
+    echo '&amp;courseId=', $course['courseId'], '">', $course['courseName'];
+    echo "</a></td>\n";
+
+    echo '<td> <button type="submit" name="deleteCourse[]"';
+    echo 'value="', $course['courseId'], '">Delete</button></td>', "\n";
+    echo "</tr>\n";
+
+    foreach($data[$course['courseId']] as $subject){
         echo "<tr><td><table border='1' style='margin-left: 25px'>\n";
-        while($subject = mysqli_fetch_array($subjects)){
-            echo '<tr>';
-            echo '<td style="width:300px"><a href="?show=addTeacher&amp;subjectName=', $subject['subjectName'];
-            echo '&amp;subjectId=', $subject['subjectId'], '">';
-            echo $subject['subjectName'], '</a></td>';
-            echo '<td> <button type="submit" name="delete[]" value="', $subject['subjectId'], '">Delete</button></td>';
-            echo "</tr>\n";
-        }
+        echo "<tr>\n";
+        echo '<td style="width:300px">';
+        echo '<a href="?show=addTeacher&amp;subjectName=';
+        echo $subject['subjectName'];
+        echo '&amp;subjectId=', $subject['subjectId'], '">';
+        echo $subject['subjectName'], '</a></td>', "\n";
+        echo '<td> <button type="submit" name="delete[]" value="';
+        echo $subject['subjectId'], '">Delete</button></td>', "\n";
+        echo "</tr>\n";
         echo "</table></td></tr>\n";
     }
 }
