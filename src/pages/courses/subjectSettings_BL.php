2179759f4221a3ad4118e26b709eb07ed2142a5b
Paul92 <paul.gafton@gmail.com>
2013-10-02 01:57:17 +0300

    Fixed bug in courseSettings

--- src/pages/courses/subjectSettings_BL.php
+++ src/pages/courses/subjectSettings_BL.php
@@ -10,20 +10,34 @@ $DB = connect();
 $data = array();
 
 $data['teachers'] = mysqli_query($DB, "SELECT * FROM teachers");
+$data['teachers'] = mysqli_fetch_all($data['teachers'], MYSQLI_ASSOC);
+
+$data['taughtBy'] = array();
+$query = "SELECT * FROM teachersToSubject WHERE subjectId=";
+$query.= $_GET['subjectId'].";";
+$teachers = mysqli_query($DB, $query);
+while ($teaches = mysqli_fetch_array($teachers)) {
+    $data['taughtBy'][] = $teaches['teacherId'];
+}
 
 $query = "SELECT * FROM subjects WHERE subjectId=".$_GET['subjectId'];
 $subject = mysqli_query($DB, $query);
 $subject = mysqli_fetch_array($subject);
 $data['subjectName'] = $subject['subjectName'];
+$data['subjectId']   = $subject['subjectId'];
 $data['hours'] = $subject['hours'];
-$data['subjectId'] = $_GET['subjectId'];
 
 if (isset($_POST['submit']) && isset($_POST['subjectId'])) {
-    foreach($_POST['teachers'] as $teacher){
-        $query = 'INSERT INTO teachersToSubject (subjectId, teacherId) VALUES (';
-        $query.= $_POST['subjectId'].', '.$teacher.');';
-    }
+    $query = "DELETE FROM teachersToSubject WHERE subjectId=";
+    $query.= $subject['subjectId'].";";
     mysqli_query($DB, $query);
+    if (isset($_POST['teachers'])) {
+        foreach ($_POST['teachers'] as $teacher) {
+            $query = 'INSERT INTO teachersToSubject (subjectId, teacherId) VALUES (';
+            $query.= $_POST['subjectId'].', '.$teacher.');';
+            mysqli_query($DB, $query);
+        }
+    }
     header('Location: ?show=courses');
 }
 
--- src/pages/courses/subjectSettings_VL.php
+++ src/pages/courses/subjectSettings_VL.php
@@ -3,10 +3,13 @@
     echo "<p>Available teachers</p>";
     echo '<form method="POST">';
     echo '<table>';
-    while ($teacher = mysqli_fetch_array($data['teachers'])) {
+    foreach ($data['teachers'] as $teacher) {
         echo '<tr><td> Teacher ', $teacher['teacherName'];
         echo '<input type="checkbox" name="teachers[]" value="';
-        echo $teacher['teacherId'], '"/>';
+        echo $teacher['teacherId'], '"';
+        if (array_search($teacher['teacherId'], $data['taughtBy']) !== FALSE) 
+            echo ' checked';
+        echo '/>';
         echo '</td></tr>';
     }
     echo '</table>';
